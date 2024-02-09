23b39a6d644e348c4ba9bca8e0bd99e201dd42df
vishnu biradar <biradarvishnu89@gmail.com>
2024-02-09 11:23:36 +0530

    Added workflows

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     module: {
       rules: [
@@ -12,7 +13,13 @@ module.exports = {
             },
           },
         },
+        
       ],
     },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+    ]
   };
   
---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,27 @@
+const { merge } = require('webpack-merge');
+
+const commonConfig = require('./webpack.common');
+const ModuleFederationPlugin =require('webpack/lib/container/ModuleFederationPlugin')
+const packageJson =require('../package.json');
+
+const domain =process.env.PRODUCTION_DOMAN;
+
+const prodConfig ={
+
+    mode :'production',
+    output :{
+
+        filename: '[name].[contenthash].js'
+    },
+    plugins :[
+        new ModuleFederationPlugin({
+            name :'container',
+            remotes:{
+                marketing: `marketing@${domain}/marketing/remoteEntry.js`
+            },
+            shared :packageJson.dependencies
+        })
+    ]
+}
+
+module.exports= merge(commonConfig,prodConfig)
